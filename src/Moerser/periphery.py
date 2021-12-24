e93677c1c1179b3c5f4c52a69db7413951843913
BennerLukas <arbeit.protonmail.com>
2021-12-24 13:16:14 +0100

    camera change algorithm

--- src/Moerser/periphery.py
+++ src/Moerser/periphery.py
@@ -1,24 +1,59 @@
-import cv2
+import cv2 as cv
 from Moerser.utils import set_logger
+import time
 
 
 class Camera:
 
     def __init__(self, mode="debug"):
+
         self.debug_level = mode
         self.log = set_logger("Moerser - Camera", mode=mode)
 
-    def image_to_morse(self, image, short=".", long="-", word_space=""):
-        pass
+        self.video_capture = cv.VideoCapture(0)
+        self.log.debug("Camera starting")
+        time.sleep(1)
+        self.returned, self.frame = self.video_capture.read()
+        time.sleep(1)
 
-    def analyze_stream(self):
-        """
-        predict what's dot and whats dash based on whole stream.
-        :return:
-        """
+    # Frame generation for Browser streaming
+    def get_frame(self):
+        returned, image = self.video_capture.read()
+        if not returned:
+            self.log.error("Can't receive frame (stream end?). Exiting ...")
+            raise
+        gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
+        return gray_image
 
-class Microphone:
-    def __init__(self, mode="debug"):
-        self.debug_level = mode
-        self.log = set_logger("Moerser - Microphone", mode=mode)
+    @staticmethod
+    def calc_mean_brightness(img):
+        return img.mean(axis=0).mean(axis=0)
+
+    def __del__(self):
+        self.video_capture.release()
+        cv.destroyAllWindows()
+        self.log.info("Camera stopped")
+        return ()
+
+    def record_sequence(self, iterations=10):
+        recorded_morse = list()
+        image = self.get_frame()
+        prev_brightness = self.calc_mean_brightness(image)
+
+        tolerance = 0.25       # change must be at least 25% compared to before
+        frequency_delay = 1
+        for i in range(iterations):
+            image = self.get_frame()
+            brightness = self.calc_mean_brightness(image)
+            if brightness > prev_brightness * (1 + tolerance):      # significantly brighter than before
+                # change from dark to bright
+                recorded_morse.append(1)
+            elif brightness < prev_brightness * (1 + tolerance):    # significantly darker than before
+                # change from bright to dark
+                recorded_morse.append(0)
+            else:
+                last_item = len(recorded_morse) - 1
+                recorded_morse.append(recorded_morse[last_item])    # no change append last status again
+            time.sleep(frequency_delay)
+        return recorded_morse
--- src/bin/exec_moerser.py
+++ src/bin/exec_moerser.py
@@ -1,18 +1,9 @@
-# import unittest
-#
-#
-# class MyTestCase(unittest.TestCase):
-#     def test_something(self):
-#         self.assertEqual(True, False)  # add assertion here
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
-
 from Moerser.decipher import Decipher
 
-morse = Decipher()
-s_input_value = ".... .. ....... .-.. ..- -.- .- ..."   # HI LUKAS
-result = morse.execute(s_input_value)
+if __name__ == '__main__':
 
-print(result)
+    morse = Decipher("info")
+    s_input_value = ".... .. ....... .-.. ..- -.- .- ..."   # HI LUKAS
+    result = morse.execute(s_input_value)
+
+    print(result)

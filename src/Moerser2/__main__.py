bb087aac06e5e6b4121a5cf23de4c93b2f93057c
Madhour <aymanmadhour@gmail.com>
2022-01-06 23:18:16 +0100

    fixed decoding

--- src/Moerser2/__main__.py
+++ src/Moerser2/__main__.py
@@ -3,53 +3,65 @@ from matplotlib import pyplot as plt
 from Moerser.utils import set_logger
 from periphery import Camera
 
-def cam():       
-    # Connect to webcam
-    cap = cv2.VideoCapture(2)
-    # Loop through every frame until we close our webcam
-    while cap.isOpened(): 
-        ret, frame = cap.read()
-        
-        # Show image 
-        cv2.imshow('Webcam', frame)
-        
-        # Checks whether q has been hit and stops the loop
-        if cv2.waitKey(1) & 0xFF == ord('q'): 
-            break
-
-    # Releases the webcam
-    cap.release()
-    # Closes the frame
-    cv2.destroyAllWindows()
-
 camera_p = Camera()
 frame_count = 0
+frame = 1 # Frame per second
 brightness_threshold = 0
-tolerance = 0.25
+tolerance = 0.05
 bright_counter = 0
+darkness_counter = 0
 sequence = ""
+total_sequence = ""
 
 while True:
     frame = cv2.imshow("webcam", camera_p.get_frame())
     frame_brightness = Camera.calc_mean_brightness(camera_p.get_frame())
     
-    #if first frame -> set avg brightness 2 threshold
+    # If first frame -> set avg brightness 2 threshold
     if frame_count == 0:
+        print(f"Threshold initiated  at {frame_brightness}")
         brightness_threshold = frame_brightness
-        
-    if frame_brightness >= brightness_threshold * (1 - tolerance):
-        bright_counter += 1
     
+    # Track brightness
+    if frame_brightness > brightness_threshold * (1 + tolerance):
+        bright_counter += 1
+        darkness_counter = 0
     else:
-        print(bright_counter)
-        if bright_counter in range(0,3):
-            sequence.append(".")
-            bright_counter == 0
-            
-        elif bright_counter > 3:
-            sequence.append("-")
-            bright_counter == 0
+        # Decoding light to morse code
+        darkness_counter += 1
+        print(f"Current Brightness Counter: {bright_counter}")
+        print(f"Current Darkness Counter: {darkness_counter}") 
         
+        # Brightness
+        if bright_counter in range(1, 3):
+            sequence += "."
+            #print(".")
+            #bright_counter = 0 
+        elif bright_counter >= 3 :
+            sequence += "-"
+            #print("-")
+            #bright_counter = 0
+        
+        # Darkness
+        if darkness_counter in range(1, 3):
+            # next character in word
+            total_sequence += sequence
+            sequence = ""
+            bright_counter = 0
+            
+        elif darkness_counter > 7 :
+            # next word
+            #total_sequence += sequence # not needed because a darkness_counter of 1 will push sequence already 
+            #sequence = ""
+            total_sequence += " "
+            bright_counter = 0
+        
+
+        
+        
+        print(f"Current sequence: {total_sequence}")
+       
+
     frame_count += 1
     
     if cv2.waitKey(1) & 0xFF == ord('q'): 

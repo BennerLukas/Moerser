998e935dff4edb24d5aa94d791323328ff454ae1
Madhour <aymanmadhour@gmail.com>
2022-02-19 00:24:43 +0100

    restructure interpreter

--- src/Moerser/__main__.py
+++ src/Moerser/__main__.py
@@ -1,82 +1,46 @@
 import cv2
+import time
 from matplotlib import pyplot as plt
 from Moerser.utils import set_logger
 from Moerser.periphery import Camera
+from Moerser.morse2text import Morse2Text
+from Moerser.interpreter import Interpreter
 
-camera_p = Camera()
+camera_p = Camera(0)
+frame_throttle = 0.5 # throttle limit
 frame_count = 0
-frames = 1  # Frame per second
-brightness_threshold = 0
-tolerance = 0.05
-bright_counter = 0
-darkness_counter = 0
 sequence = ""
 total_sequence = " "
+decoded_sequence = " "
+decoder = Morse2Text()
+interpreter = Interpreter()
+startTime = time.time()
+
 
 while True:
+    nowTime = time.time()
     current_frame = camera_p.get_frame()
     frame_brightness = Camera.calc_mean_brightness(current_frame)
 
     # If first frame -> set avg brightness 2 threshold
     if frame_count == 0:
-        print(f"Threshold initiated  at {frame_brightness}")
-        brightness_threshold = frame_brightness
+        interpreter.set_baseline(current_frame)
 
-    # Track brightness
-    if frame_brightness > brightness_threshold * (1 + tolerance):
-        bright_counter += 1
-        darkness_counter = 0
-        print(f"Current Brightness Counter: {bright_counter}")
-        print(f"Current Darkness Counter: {darkness_counter}")
-
-    else:
-        # Decoding light to morse code
-        darkness_counter += 1
-        print(f"Current Brightness Counter: {bright_counter}")
-        print(f"Current Darkness Counter: {darkness_counter}")
-
-        # Brightness
-        if bright_counter in range(1, (5 * frames)):
-            sequence = "."
-            # print(".")
-            # bright_counter = 0
-        elif bright_counter >= (5 * frames):
-            sequence = "-"
-            # print("-")
-            # bright_counter = 0
-
-        # Darkness
-        if darkness_counter in range(1, (7 * frames)):
-            # next character in word
-            total_sequence += sequence
-            sequence = ""
-            bright_counter = 0
-
-        elif darkness_counter > (7 * frames):
-            # next word
-            # total_sequence += sequence # not needed because a darkness_counter of 1 will push sequence already
-            # sequence = ""
-            if total_sequence[-1] != " ":
-                total_sequence += " "
-
-            bright_counter = 0
-        
-
-        
-        
-        print(f"Current sequence: {total_sequence}")
+    if (nowTime - startTime) > frame_throttle:
+        print(nowTime - startTime)
+        sequence = interpreter.check_brightness(frame_brightness)
+        total_sequence = interpreter.total_sequence
+        decoded_sequence = decoder.decode(total_sequence)
+        startTime = time.time() # reset time
        
-
     frame_count += 1
 
     cv2.putText(current_frame, f"Morse_Code: {total_sequence}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-    # TODO: After space, translate current morse code
-    # TODO: show current sequence in frame
-    # TODO: Button to initiate avg baseline
+    cv2.putText(current_frame, f"Decoded_Sequence: {decoded_sequence}", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.putText(current_frame, f"Current_sequence: {sequence}", (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
     cv2.imshow("webcam", current_frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    cv2.waitKey(500)  # one frame per second
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+    #    break
+    cv2.waitKey(10)  # one frame per second
